75b9d01ef00d8c2d0ff1d767beb4b114e229f338
Thomas Campistron <irevoire@hotmail.fr>
2020-03-06 11:26:37 +0100

    question 3

--- chapter1.ml
+++ chapter1.ml
@@ -8,4 +8,7 @@ let rec _update_budget budget expenses =
         | head::tail -> _update_budget (budget - head) tail
 
 (* Question 2 *)
-let length l = List.fold_left (fun acc _el -> acc + 1) 0 l;;
+let length l = List.fold_left (fun acc _el -> acc + 1) 0 l
+
+(* Question 3 *)
+let last l = List.fold_left (fun _acc el -> Some(el)) None l;;
